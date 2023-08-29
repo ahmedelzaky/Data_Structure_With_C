4ae38c2f76bfcd95382c5bc4cdeed39340295e76
ahmedelzaky <ahmed.elzakistudye@gmail.com>
2023-08-29 14:46:19 +0300

    add Makefiles

--- StackAndQueue/main.c
+++ StackAndQueue/main.c
@@ -17,24 +17,24 @@ int main()
         enQueue(myQueue, i);
     }
 
-    printf("Size of Queue : %d\n", myQueue->size);
-    printf("capacity of Queue : %d\n", myQueue->capacity);
+    printf("Size of Queue : %ld\n", myQueue->size);
+    printf("capacity of Queue : %ld\n", myQueue->capacity);
     QueueFront(myQueue, &data);
     printf("First : %d\n", data);
     QueueLast(myQueue, &data);
     printf("Last : %d\n", data);
 
     //  getchar();
-
+    printf("[");
     while (!isQueueEmpty(myQueue))
     {
         deQueue(myQueue, &data);
-        printf("|%d| ", data);
+        printf(" %d,", data);
     }
+    printf("\b]");
 
     freeQueue(myQueue);
     puts("\n<==========Finish Queue Test==========>");
-
 
     puts("\n<==========Start Stack Test==========>");
 
@@ -45,18 +45,19 @@ int main()
         push(myStack, i);
     }
 
-    printf("Size of Stack : %d\n", getStackSize(myStack));
-    printf("capacity of Queue : %d\n", myStack->capacity);
+    printf("Size of Stack : %ld\n", getStackSize(myStack));
+    printf("capacity of Queue : %ld\n", myStack->capacity);
     top(myStack, &data);
     printf("Top : %d\n", data);
 
     //  getchar();
-
+    printf("[");
     while (!isStackEmpty(myStack))
     {
         pop(myStack, &data);
-        printf("|%d| ", data);
+        printf(" %d,", data);
     }
+    printf("\b]");
 
     freeStack(myStack);
 
